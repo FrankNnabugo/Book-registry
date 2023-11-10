6c41b24e2a6aaeaf287d37faf2ad48d64dae407c
FrankNnabugo <franksamuel224@gmail.com>
2023-11-10 02:37:55 -0800

    setup a port

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 
+//declare a variable app and assign a function to it. 
 const app = express();
 
+const {dbconnect} = require("./dbconnect/dbconnect");
+
 app.listen(3000, ()=>{
-    console.log("server is ruunng on port 3000");
+
+    console.log("server is running on port 3000");
 });
+
+//intialize dbconnect
+dbconnect();
