82017680788f8e403beb4fa6aea90e7cab45d8de
FrankNnabugo <franksamuel224@gmail.com>
2023-11-22 05:53:24 -0800

    refactored server code

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 const express = require("express");
 
-//declare a variable app and assign a function to it. 
-const app = express();
+const mongoose = require("mongoose");
+
+const route = require("./routes/book.route");
 
 const {dbconnect} = require("./dbconnect/dbconnect");
 
-app.listen(3000, ()=>{
+ const PORT = process.env.PORT || 4000;
 
-    console.log("server is running on port 3000");
+//init express
+const app = express();
+
+//setting up middleware-  
+
+app.use(express.json())
+
+app.use((req, res, next)=>{
+console.log(req.path, req.method)
+next()
+
 });
 
-//intialize dbconnect
-dbconnect();
+//invoking route
+
+app.use("/book", route);    
+
+
+//listening to port
+app.listen(process.env.PORT || 4000, ()=>{
+
+    console.log("server is running on port", process.env.PORT);
+});
